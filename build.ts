1a5a43ca8528533e55ec5c99e53d8e221d32cb47
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-12-26 14:55:32 +0100

    ts

---
+++ build.ts
@@ -0,0 +1,7 @@
+import root from './zotero-webpack/root'
+process.chdir(root)
+
+import './zotero-webpack/make-dirs'
+import './zotero-webpack/copy-assets'
+import './zotero-webpack/rdf'
+import './zotero-webpack/version'
---
+++ webpack.config.ts
@@ -0,0 +1,65 @@
+// tslint:disable:no-console
+
+// const replace = require('replace')
+
+import * as webpack from 'webpack'
+import * as path from 'path'
+
+import BailPlugin from './zotero-webpack/plugin/bail'
+
+import CircularDependencyPlugin = require('circular-dependency-plugin')
+// import AfterBuildPlugin = require('./zotero-webpack/plugin/after-build')
+
+const config = {
+  node: { fs: 'empty' },
+  resolveLoader: {
+    alias: {
+      'pegjs-loader': path.join(__dirname, './zotero-webpack/loader/pegjs.ts'),
+      'json-loader': path.join(__dirname, './zotero-webpack/loader/json.ts'),
+      'wrap-loader': path.join(__dirname, './zotero-webpack/loader/wrap.ts'),
+      'bcf-loader': path.join(__dirname, './setup/loaders/bcf.ts'),
+    },
+  },
+  module: {
+    rules: [
+      { test: /\.pegjs$/, use: [ 'pegjs-loader' ] },
+      { test: /\.json$/, use: [ 'json-loader' ] },
+      { test: /\.bcf$/, use: [ 'bcf-loader' ] },
+      { test: /\.ts$/, exclude: [ /node_modules/ ], use: [ 'wrap-loader', 'ts-loader' ] },
+    ],
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new CircularDependencyPlugin({ failOnError: true }),
+    /*
+    new AfterBuildPlugin((stats, options) => {
+      const ccp = options.plugins.find(plugin => plugin instanceof webpack.optimize.CommonsChunkPlugin).filenameTemplate
+      replace({
+        regex: `window\\["${options.output.jsonpFunction}"\\]`,
+        replacement: options.output.jsonpFunction,
+        paths: [path.join(options.output.path, ccp)],
+      })
+    }),
+    */
+    BailPlugin,
+  ],
+
+  context: path.resolve(__dirname, './content'),
+
+  entry: {
+    StorageScanner: './StorageScanner.ts',
+  },
+
+  output: {
+    path: path.resolve(__dirname, './build/content'),
+    filename: '[name].js',
+    jsonpFunction: 'Zotero.WebPackedStorageScanner',
+    devtoolLineToLine: true,
+    pathinfo: true,
+    library: 'Zotero.[name]',
+    libraryTarget: 'assign',
+  },
+}
+
+export default config
