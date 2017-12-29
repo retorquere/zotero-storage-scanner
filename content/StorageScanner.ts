59d28f7646f1fb3559134e199dfc2261e5afab45
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-12-29 15:14:47 +0100

    content/StorageScanner.ts

--- content/StorageScanner.ts
+++ content/StorageScanner.ts
@@ -1,31 +1,56 @@
 declare const Zotero: any
 
 export = new class StorageScanner {
-  /*
+  private query: string
+
   constructor() {
     window.addEventListener('load', e => { this.load() }, false)
   }
 
   public async load() {
+    if (!this.query) {
+      // Zotero startup is a hot mess; https://groups.google.com/d/msg/zotero-dev/QYNGxqTSpaQ/uvGObVNlCgAJ
+      await Zotero.Schema.schemaUpdatePromise
+
+      let attachmentTypeID
+      for (const type of await Zotero.DB.queryAsync("select itemTypeID from itemTypes where typeName = 'attachment'")) {
+        attachmentTypeID = type.itemTypeID
+      }
+
+      this.query = `
+        WITH duplicates AS (
+          SELECT parentItemID, contentType, COUNT(*) as duplicates
+          FROM itemAttachments
+          WHERE linkMode <> ${Zotero.Attachments.LINK_MODE_LINKED_URL} AND itemID NOT IN (select itemID from deletedItems)
+          GROUP BY parentItemID, contentType
+        )
+        SELECT item.itemID, attachment.path, COALESCE(duplicates.duplicates, 1) as duplicates
+        FROM items item
+        LEFT JOIN itemAttachments attachment ON attachment.itemID = item.itemID
+        LEFT JOIN duplicates on attachment.parentItemID = duplicates.parentItemID AND attachment.contentType = duplicates.contentType
+        WHERE
+            item.itemTypeID = ${attachmentTypeID}
+          AND
+            item.itemID NOT IN (select itemID from deletedItems)
+          AND
+            (
+                attachment.path IS NULL
+              OR
+                (attachment.linkMode IS NOT NULL AND attachment.linkMode <> ${Zotero.Attachments.LINK_MODE_LINKED_URL})
+            )
+      `.replace(/\n/g, ' ').trim()
+    }
   }
-  */
 
   public async scan() {
-    // Zotero startup is a hot mess; https://groups.google.com/d/msg/zotero-dev/QYNGxqTSpaQ/uvGObVNlCgAJ
-    await Zotero.Schema.schemaUpdatePromise
+    await this.load()
 
-    const attachments = await Zotero.DB.queryAsync(`
-      WITH duplicates AS (
-        SELECT parentItemID, contentType, COUNT(*) as duplicates
-        FROM itemAttachments
-        WHERE itemID NOT IN (select itemID from deletedItems)
-        GROUP BY parentItemID, contentType
-      )
-      SELECT itemAttachments.itemID, COALESCE(duplicates.duplicates, 1) as duplicates
-      FROM itemAttachments
-      LEFT JOIN duplicates on itemAttachments.parentItemID = duplicates.parentItemID AND itemAttachments.contentType = duplicates.contentType
-      WHERE itemID NOT IN (select itemID from deletedItems)
-    `.replace(/\n/g, ' ').trim())
+    // Zotero.Attachments.LINK_MODE_LINKED_URL // ignore this
+    // Zotero.Attachments.LINK_MODE_IMPORTED_URL // snapshot
+    // Zotero.Attachments.LINK_MODE_IMPORTED_FILE
+    // LINK_MODE_LINKED_FILE
+
+    const attachments = await Zotero.DB.queryAsync(this.query)
 
     for (const attachment of attachments) {
       const item = await Zotero.Items.getAsync(attachment.itemID)
